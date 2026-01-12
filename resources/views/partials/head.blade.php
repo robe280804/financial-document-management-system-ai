336c8009ef114fef93f4b785c89f442616d85303
Roberto Sodini <robertosodini1@gmail.com>
2026-01-12 15:06:30 +0100

    feat: Dashboard field

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,7 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" href="/images/finance-economics.svg" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
